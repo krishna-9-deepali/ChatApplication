da10c1b9fcee3822018bf3af603e6885459f8fb9
Deepali Dave <147068343+krishna-9-deepali@users.noreply.github.com>
2024-09-02 20:00:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
   origin: process.env.FRONTEND_URL,
-  methods: ["POST", "GET"],
+  methods: ["POST", "GET","OPTIONS"],
   credentials: true,
 };
 app.use(cors(corsOption));
